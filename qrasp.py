b343ace560ad820a8cf79777e768496c3a9a2fef
JanLahmann <jan.lahmann@de.ibm.com>
2021-08-14 22:39:47 +0200

    fix exit

--- qrasp.py
+++ qrasp.py
@@ -293,10 +293,10 @@ def pushed_middle(event):
         hat.clear()
         atexit.register(call_sense_menu)
         os.kill(os.getpid(), signal.SIGINT)
-        cmd="sleep 2 && kill -INT " + str(os.getpid()) + "\n sleep 2 && kill -TERM " + str(os.getpid())
-        with open('cmd.sh', 'w') as f:
-            print(cmd, file=f)  
-        os.system("nohup sh cmd.sh &")
+#        cmd="sleep 2 && kill -INT " + str(os.getpid()) + "\n sleep 2 && kill -TERM " + str(os.getpid())
+#        with open('cmd.sh', 'w') as f:
+#            print(cmd, file=f)  
+#        os.system("nohup sh cmd.sh &")
         exit()
     if event.action == ACTION_PRESSED:
         print("Middle ACTION_PRESSED")
